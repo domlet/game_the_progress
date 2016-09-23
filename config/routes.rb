0caa9bd9d6ae85861dadee9e33acddbb2d8b30d4
Dom Brassey <dombrassey@gmail.com>
2016-09-23 16:09:16 -0700

    Very view routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :games, only: [:index, :show]
+  resources :results, only: [:new]
+
+  root to: 'games#index'
+
 end
