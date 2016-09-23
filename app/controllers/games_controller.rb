77e2eebc42346c6a0f7c87793899929bdc5d7841
Dom Brassey <dombrassey@gmail.com>
2016-09-23 15:53:01 -0700

    Add methods to controllers

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,8 @@
 class GamesController < ApplicationController
+
+  def show
+    @game = Game.find_by(id: params[:id])
+    @results = Result.new
+  end
+
 end
--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,2 +1,18 @@
 class ResultsController < ApplicationController
+
+  def create
+    result = Result.new(result_params)
+    result.user = current_user
+    if result.save
+      # do some stuff
+    else
+      redirect_to games_path
+    end
+  end
+
+  private
+
+    def result_params
+      params.require(:result).permit(:content, :game_id)
+    end
 end
