72d6a8854bf286f2e518f0e54164718ebe7c92eb
Frank Aiello & Ryker <pair+eyeyellow.reedr3@devbootcamp.com>
2016-09-24 11:13:26 -0700

    Resolved merge conflicts from devise migration conflicts - FA RR DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160924033412) do
 
   # These are extensions that must be enabled in order to support this database
@@ -55,14 +54,12 @@ ActiveRecord::Schema.define(version: 20160924033412) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-
     t.datetime "created_at",                            null: false
     t.datetime "updated_at",                            null: false
     t.string   "username"
     t.string   "first_name"
     t.string   "last_name"
     t.boolean  "is_admin",               default: true
-
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
