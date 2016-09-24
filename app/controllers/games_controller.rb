ce253bd0c98e78af55935123ba76413cb1f768a5
Dom Brassey <dombrassey@gmail.com>
2016-09-24 15:22:27 -0700

    Remove roo before merge with dev

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,3 @@
 class GamesController < ApplicationController
 
-  def import
-    Game.import(params[:file])
-    redirect_to root_url, notice: "Game imported."
-  end
-
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,23 +1,3 @@
 class Game < ApplicationRecord
 
-  def self.import(file)
-    spreadsheet = open_spreadsheet(file)
-    header = spreadsheet.row(1)
-    (2..spreadsheet.last_row).each do |i|
-      row = Hash[[header, spreadsheet.row(i)].transpose]
-      game = find_by_id(row["id"]) || new
-      game.attributes = row.to_hash.slice(*accessible_attributes)
-      game.save!
-    end
-  end
-
-  def self.open_spreadsheet(file)
-    case File.extname(file.original_filename)
-    when ".csv" then Csv.new(file.path, nil, :ignore)
-    when ".xls" then Excel.new(file.path, nil, :ignore)
-    when ".xlsx" then Excelx.new(file.path, nil, :ignore)
-    else raise "Unknown file type: #{file.original_filename}"
-    end
-  end
-
 end
