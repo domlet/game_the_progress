5b5720aed64d55d397149b3694d26179ab0fd798
Dom Brassey <dombrassey@gmail.com>
2016-09-24 20:26:38 -0700

    Get Roo import method on ImportedGamesController working.

--- app/controllers/imported_games_controller.rb
+++ app/controllers/imported_games_controller.rb
@@ -1,9 +1,18 @@
 class ImportedGamesController < ApplicationController
   def import
-    ImportedGame.import(params[:file])
+    s = Roo::CSV.new("./public/spreadsheets/test.csv")
+    p "*" * 50
+    p s
+    p s.cell(1, 1)
+    p "*" * 50
+    # p ImportedGame.import(params[:file])
     redirect_to root_url, notice: "ImportedGames imported."
   end
 
   def index
   end
+
+
+
+
 end
