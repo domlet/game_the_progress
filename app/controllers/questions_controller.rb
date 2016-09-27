b63a43ae422f3c5d486f9cad6085feb9e3c724a8
Dom Brassey & Ryker <pair+domlet.reedr3@devbootcamp.com>
2016-09-26 19:06:23 -0700

    Update seeds to provide complete data for test game 1

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,21 +1,13 @@
 class QuestionsController < ApplicationController
 
   def update
-    p "We hit the route!!!"
+    p "We hit the update route!!!"
     p params
     p "*" * 50
     @game = Game.find(params[:id])
-    @questions = @game.questions
-    current_question_id = (params.key('current_question_id')).to_i
-    current_question = Question.find(current_question_id)
-    current_index = @questions.find_index(current_question)
-
-    # Enables first question to be answered
-    if current_index == 0
-      @question = @questions[current_index]
-    else
-      @question = @questions[current_index + 1]
-    end
+    # Find the id of button that was clicked
+    current_question_id = (params.key('hidden-thing')).to_i
+    @question = Question.find(current_question_id)
 
     # This is publisher (sends stuff out)
     # Triggered when 'next' button is clicked
@@ -27,3 +19,14 @@ class QuestionsController < ApplicationController
   end
 
 end
+
+
+
+    # current_index = @questions.find_index(current_question)
+
+    # # Enables first question to be answered
+    # if current_index == 0
+    #   @question = @questions[current_index]
+    # else
+    #   @question = @questions[current_index + 1]
+    # end
