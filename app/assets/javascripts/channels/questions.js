e754c52f33e3c6d9ef2ac84a1780d9ed5bd2c8ab
Dom Brassey & Ryker <pair+domlet.reedr3@devbootcamp.com>
2016-09-26 20:11:53 -0700

    Hide and show student answer buttons + add 'a,b,c,d' formatting

--- app/assets/javascripts/channels/questions.js
+++ app/assets/javascripts/channels/questions.js
@@ -7,21 +7,25 @@ App.questions = App.cable.subscriptions.create('QuestionsChannel', {
     $("#questions").removeClass('hidden'); // Unknown
     $('#questions').html(this.renderQuestion(data)); // Renders the new question
     $('#answers').html(this.renderAnswers(data)); // Renders the new answers
+    // Hide and show 'Next' button
     $('.next-button').hide();
     $("#button-" + (data.question_id + 1)).show();
+    // Hide and show 'Results' buttons
+    $('.results-buttons').hide();
+    $("#results-" + (data.question_id)).show();
   },
 
   renderQuestion: function(data) {
     console.log('question rendered');
-    return "<p>" + data.question_text + "</p>";
+    return "<p>" + data.question_id + ". " + data.question_text + "</p>";
   },
 
   renderAnswers: function(data) {
     console.log('answer rendered');
-    return ("<p>" + data.answer_options[0].text + "</p>" +
-            "<p>" + data.answer_options[1].text + "</p>" +
-            "<p>" + data.answer_options[2].text + "</p>" +
-            "<p>" + data.answer_options[3].text + "</p>")
+    return ("<p>A. " + data.answer_options[0].text + "</p>" +
+            "<p>B. " + data.answer_options[1].text + "</p>" +
+            "<p>C. " + data.answer_options[2].text + "</p>" +
+            "<p>D. " + data.answer_options[3].text + "</p>")
   }
 
 });
