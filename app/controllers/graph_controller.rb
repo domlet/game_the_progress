4a8fb6937cfcff91f690e25c17849a14da2aff24
Maria Uyeda <email": null>
2016-09-27 21:56:06 -0700

    Create method to return percentage of class results for game

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -1,44 +1,98 @@
 class GraphController < ApplicationController
 
-  # Method to show results for entire class
+  # Method to show results for all students on teacher view
+  # route: /graph/students
   def students
-    # @results = Result.all
-    # #  p @results
-    # boolean_array = @results.map do |result|
-    #   result.is_correct
-    # end
-    # true_array = boolean_array.select {|boolean| boolean == true}
-    # true_count = true_array.length
-    # false_array = boolean_array.select {|boolean| boolean == false}
-    # false_count = false_array.length
-    # p true_count
-    # p false_count
-    # p boolean_array
-
-    data = {
+    @results = Result.all
+    boolean_array = @results.map do |result|
+      result.is_correct
+    end
+    true_array = boolean_array.select {|boolean| boolean == true}
+    true_count = (true_array.length * 100)/5
+    false_array = boolean_array.select {|boolean| boolean == false}
+    false_count = false_array.length
+    p "*" * 20
+    p true_count
+     data = {
           clickable: false,
           data: [
-                  {label: "name1", value: 8, id: 1},
-                  {label: "name2", value: 3, id: 1},
-                  {label: "name3", value: 2, id: 1},
-                  {label: "name4", value: 6, id: 1},
-                  {label: "name5", value: 1, id: 1},
-                  {label: "name6", value: 7, id: 1}
+                  {label: "Correct", value: true_count, id: 1},
+                  {label: "Incorrect", value: false_count, id: 1},
+
                 ]
           }
-          p data
+          # p "*" * 20
+          # p data
     @data = data.to_json
-          p @data
-  # Return JSON object of whole class student
+
+     ################ PRACTICE CODE ##################################
+ # @users = User.all
+ #    if params[:id] != "index"
+ #      @user = User.find(params[:id])
+ #      true_count = 0
+ #      false_count = 0
+ #      @user.results.each do |result|
+ #        if result.is_correct == true
+ #          true_count += 1
+ #        else
+ #          false_count += 1
+ #        end
+ #      end
+
+ #     end
+ #      data = {
+ #          clickable: false,
+ #          data: [
+ #                  {label: "Correct", value: true_count},
+ #                  {label: "Incorrect", value: false_count}
+ #                ]
+ #              }
+ #     @data = data.to_json
+     ################ START FRESH CODE ##################################
+    # @users = User.all
+    # if params[:id] != "index"
+    #   @user = User.find(params[:id])
+    #   true_count = 0
+    #   false_count = 0
+    #   @users.results.each do |result|
+    #     if result.is_correct == true_count
+    #       true_count += 1
+    #     else
+    #       false_count += 1
+    #     end
+
+    #   data = {
+    #     clickable: false,
+    #     data: [
+    #             { label: user.first_name, value: true_count, id: user.id }
+    #     ]
+    #   }
+     # @data = data.to_json
+    # end
+    # end
+
+
+####### HARDCODED FOR GRAPH ##############
+    # data = {
+    #       clickable: false,
+    #       data: [
+    #               {label: "name1", value: 8, id: 1},
+    #               {label: "name2", value: 3, id: 1},
+    #               {label: "name3", value: 2, id: 1},
+    #               {label: "name4", value: 6, id: 1},
+    #               {label: "name5", value: 1, id: 1},
+    #               {label: "name6", value: 7, id: 1}
+    #             ]
+    #       }
+    #       p data
+    # @data = data.to_json
+####### END HARDCODED FOR GRAPH ##############
 
   end
 
   # Method to show individual student results
   def show
     @users = User.all
-    # p "*" * 88
-    # p params
-    # p params[:id]
     if params[:id] != "index"
       @user = User.find(params[:id])
       true_count = 0
@@ -59,23 +113,8 @@ class GraphController < ApplicationController
                   {label: "Incorrect", value: false_count}
                 ]
               }
-     p "*" * 20
-     p data
      @data = data.to_json
-     p "8" * 20
-     p @data
 
-     # Hardcoding of data to make graphs work
-    #     data = {
-    #       clickable: false,
-    #       data: [
-    #               {label: "Good", value: 8, id: 1},
-    #               {label: "Bad", value: 3, id: 1}
-    #             ]
-    #       }
-    #       p data
-    # @data = data.to_json
-    #       p @data
   end
 end
 
